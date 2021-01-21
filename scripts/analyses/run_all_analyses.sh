eb1938f7b9cb0b6ce9431bd34a2805600463c0a2
ahnishida <ahnishida@utexas.edu>
2021-01-21 09:55:30 -0600

    add gzip asv table

--- scripts/analyses/run_all_analyses.sh
+++ scripts/analyses/run_all_analyses.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+#gunzip results/gyrb/inputs/physeq_Bacteroidales_asv_tab.txt.gz
+#set current working dir in scripts 
 ipython scripts/analyses/16S_ASV_sharing_HRtype.ipynb
   #loads functions.ipynb to assign HR ASVs
 Rscript scripts/analyses/16S_analyses_all.R
-#ipython scripts/analyses/gyrb_hr_clades.ipynb
-#Rscript scripts/analyses/gyrb_visualize_all.R
+ipython scripts/analyses/gyrb_hr_clades.ipynb
+Rscript scripts/analyses/gyrb_visualize_all.R
